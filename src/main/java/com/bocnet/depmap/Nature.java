8c28f98b9b245c9c2ebcf89bd83164e7ce4d9bcd
Hsi Fun <manbaum@hotmail.com>
2014-09-30 23:18:07 +0800

    Use 'BuilderUtil' to simplify code, and add JavaDoc to source.

--- src/main/java/com/bocnet/depmap/Nature.java
+++ src/main/java/com/bocnet/depmap/Nature.java
@@ -1,11 +1,30 @@
+/**
+ * !(#) Nature.java
+ * Copyright (c) 2014 Bank of China Co. Ltd.
+ * All rights reserved.
+ *
+ * Create by manbaum.
+ * On Sep 30, 2014.
+ */
+
 package com.bocnet.depmap;
 
-import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
+import com.bocnet.plugin.util.BuilderUtil;
+
+/**
+ * A class for project nature runtime classes. It can configure a project with
+ * the project nature, or de-configure it. When a project is configured with a
+ * project nature, this is recorded in the list of project natures on the
+ * project. Individual project natures may expose a more specific runtime type,
+ * with additional API for manipulating the project in a nature-specific way.
+ * 
+ * @author manbaum
+ * @since Sep 30, 2014
+ */
 public class Nature implements IProjectNature {
 
 	/**
@@ -15,52 +34,57 @@ public class Nature implements IProjectNature {
 
 	private IProject project;
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Configures this nature for its project. This is called by the workspace
+	 * when natures are added to the project using
+	 * <code>IProject.setDescription</code> and should not be called directly by
+	 * clients. The nature extension id is added to the list of natures before
+	 * this method is called, and need not be added here. Exceptions thrown by
+	 * this method will be propagated back to the caller of
+	 * <code>IProject.setDescription</code>, but the nature will remain in the
+	 * project description.
+	 * 
+	 * @author manbaum
+	 * @since Sep 30, 2014
+	 * 
+	 * @throws CoreException
+	 *             if this method fails.
 	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#configure()
 	 */
 	public void configure() throws CoreException {
-		IProjectDescription desc = project.getDescription();
-		ICommand[] commands = desc.getBuildSpec();
-
-		for (int i = 0; i < commands.length; ++i) {
-			if (commands[i].getBuilderName().equals(Builder.BUILDER_ID))
-				return;
-		}
-
-		ICommand[] newCommands = new ICommand[commands.length + 1];
-		System.arraycopy(commands, 0, newCommands, 0, commands.length);
-		ICommand command = desc.newCommand();
-		command.setBuilderName(Builder.BUILDER_ID);
-		newCommands[newCommands.length - 1] = command;
-		desc.setBuildSpec(newCommands);
-		project.setDescription(desc, null);
+		BuilderUtil.addBuilder(project, Builder.BUILDER_ID);
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * De-configures this nature for its project. This is called by the
+	 * workspace when natures are removed from the project using
+	 * <code>IProject.setDescription</code> and should not be called directly by
+	 * clients. The nature extension id is removed from the list of natures
+	 * before this method is called, and need not be removed here. Exceptions
+	 * thrown by this method will be propagated back to the caller of
+	 * <code>IProject.setDescription</code>, but the nature will still be
+	 * removed from the project description.
+	 * 
+	 * @author manbaum
+	 * @since Sep 30, 2014
+	 * 
+	 * @throws CoreException
+	 *             if this method fails.
 	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#deconfigure()
 	 */
 	public void deconfigure() throws CoreException {
-		IProjectDescription description = getProject().getDescription();
-		ICommand[] commands = description.getBuildSpec();
-		for (int i = 0; i < commands.length; ++i) {
-			if (commands[i].getBuilderName().equals(Builder.BUILDER_ID)) {
-				ICommand[] newCommands = new ICommand[commands.length - 1];
-				System.arraycopy(commands, 0, newCommands, 0, i);
-				System.arraycopy(commands, i + 1, newCommands, i,
-						commands.length - i - 1);
-				description.setBuildSpec(newCommands);
-				project.setDescription(description, null);
-				return;
-			}
-		}
+		BuilderUtil.removeBuilder(project, Builder.BUILDER_ID);
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Returns the project to which this project nature applies.
+	 * 
+	 * @author manbaum
+	 * @since Sep 30, 2014
+	 * 
+	 * @return the project handle
 	 * 
 	 * @see org.eclipse.core.resources.IProjectNature#getProject()
 	 */
@@ -68,15 +92,21 @@ public class Nature implements IProjectNature {
 		return project;
 	}
 
-	/*
-	 * (non-Javadoc)
+	/**
+	 * Sets the project to which this nature applies. Used when instantiating
+	 * this project nature runtime. This is called by
+	 * <code>IProject.create()</code> or <code>IProject.setDescription()</code>
+	 * and should not be called directly by clients.
 	 * 
-	 * @see
-	 * org.eclipse.core.resources.IProjectNature#setProject(org.eclipse.core
-	 * .resources.IProject)
+	 * @author manbaum
+	 * @since Sep 30, 2014
+	 * 
+	 * @param project
+	 *            the project to which this nature applies
+	 * 
+	 * @see org.eclipse.core.resources.IProjectNature#setProject(org.eclipse.core.resources.IProject)
 	 */
 	public void setProject(IProject project) {
 		this.project = project;
 	}
-
 }
