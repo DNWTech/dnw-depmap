fa3a62ef39d79a044b15ab575960f9b99ac77290
manbaum <manbaum@hotmail.com>
2014-10-10 11:54:43 +0800

    Add return value to indicate each operation success or not.

--- src/main/java/com/dnw/plugin/util/NatureUtil.java
+++ src/main/java/com/dnw/plugin/util/NatureUtil.java
@@ -20,7 +20,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 
 /**
- * Class/Interface NatureUtil.
+ * Give a set of methods to manipulate the project nature.
  * 
  * @author manbaum
  * @since Feb 1, 2013
@@ -28,24 +28,31 @@ import org.eclipse.core.runtime.IStatus;
 public class NatureUtil {
 
 	/**
-	 * Method addNature.
+	 * Add the given nature to a project.
 	 * 
 	 * @author manbaum
 	 * @since Feb 1, 2013
 	 * 
 	 * @param project
+	 *            the project to add nature.
 	 * @param natureId
-	 * @param checkExistance
+	 *            the nature ID.
+	 * @param checkExistence
+	 *            before adding, whether check the existence of the nature to
+	 *            add.
+	 * @return <code>true</code> if the operation has been done, else
+	 *         <code>false</code>.
 	 * @throws CoreException
+	 *             if fails.
 	 */
-	public static void addNature(IProject project, String natureId,
-			boolean checkExistance) throws CoreException {
+	public static boolean addNature(IProject project, String natureId,
+			boolean checkExistence) throws CoreException {
 		IProjectDescription description = project.getDescription();
 		String[] natures = description.getNatureIds();
-		if (checkExistance) {
+		if (checkExistence) {
 			for (String n : natures) {
 				if (n.equals(natureId))
-					return;
+					return false;
 			}
 		}
 		String[] newNatures = new String[natures.length + 1];
@@ -56,36 +63,47 @@ public class NatureUtil {
 		if (status.getCode() == IStatus.OK) {
 			description.setNatureIds(newNatures);
 			project.setDescription(description, null);
+			return true;
 		} else
 			throw new CoreException(status);
 	}
 
 	/**
-	 * Method addNature.
+	 * Add the given nature to a project.
 	 * 
 	 * @author manbaum
 	 * @since Feb 1, 2013
 	 * 
 	 * @param project
+	 *            the project to add nature.
 	 * @param natureId
+	 *            the nature ID.
+	 * @return <code>true</code> if the operation has been done, else
+	 *         <code>false</code>.
 	 * @throws CoreException
+	 *             if fails.
 	 */
-	public static void addNature(IProject project, String natureId)
+	public static boolean addNature(IProject project, String natureId)
 			throws CoreException {
-		addNature(project, natureId, true);
+		return addNature(project, natureId, true);
 	}
 
 	/**
-	 * Method removeNature.
+	 * Remove the given nature from a project.
 	 * 
 	 * @author manbaum
 	 * @since Feb 1, 2013
 	 * 
 	 * @param project
+	 *            the project to remote nature.
 	 * @param natureId
+	 *            the nature ID.
+	 * @return <code>true</code> if the operation has been done, else
+	 *         <code>false</code>.
 	 * @throws CoreException
+	 *             if fails.
 	 */
-	public static void removeNature(IProject project, String natureId)
+	public static boolean removeNature(IProject project, String natureId)
 			throws CoreException {
 		IProjectDescription description = project.getDescription();
 		String[] natures = description.getNatureIds();
@@ -100,29 +118,35 @@ public class NatureUtil {
 				if (status.getCode() == IStatus.OK) {
 					description.setNatureIds(newNatures);
 					project.setDescription(description, null);
-					return;
+					return true;
 				} else
 					throw new CoreException(status);
 			}
 		}
+		return false;
 	}
 
 	/**
-	 * Method toggleNature.
+	 * Toggle the existence of the given nature on a project. i.e. if the nature
+	 * exists, remote it; if the nature does not exist, add it.
 	 * 
 	 * @author manbaum
 	 * @since Feb 1, 2013
 	 * 
 	 * @param project
+	 *            the project to toggle nature.
 	 * @param natureId
+	 *            the nature ID.
+	 * @return <code>true</code> if the operation has been done, else
+	 *         <code>false</code>.
 	 * @throws CoreException
+	 *             if fails.
 	 */
-	public static void toggleNature(IProject project, String natureId)
+	public static boolean toggleNature(IProject project, String natureId)
 			throws CoreException {
-		if (project.hasNature(natureId)) {
-			removeNature(project, natureId);
-		} else {
-			addNature(project, natureId, false);
-		}
+		if (project.hasNature(natureId))
+			return removeNature(project, natureId);
+		else
+			return addNature(project, natureId, false);
 	}
 }
